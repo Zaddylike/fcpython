7e1cb03469ca67c1af94bb4d9201a5bc6fc68b62
Zaddylike <79909708+Zaddylike@users.noreply.github.com>
2021-03-16 19:56:47 +0800

    Add files via upload

--- buble sorted.py
+++ buble sorted.py
@@ -1,7 +1,7 @@
 data=[10,20,90,5,1,7]
 n=len(data)
-for i in range(n-1):
-    for j in range(n-i-1):
+for i in range(n-1): # 掃描一趟的總次數
+    for j in range(n-i-1): #每一趟掃描交換的次數
         if data[j]>data[j+1]:
             data[j],data[j+1]=data[j+1],data[j]
 print(data)
